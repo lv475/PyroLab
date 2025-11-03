09f008e52cd3c1e3bcd14b5f607bbe5f723ce4bf
levchenko viktoria <levchenkovika475@gmail.com>
2025-11-03 19:31:57 +1000

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,53 +2,115 @@
   <main class="main_page">
     <div class="container">
       <div class="main_photo">
-        <img src="" alt="">
+        <img src="../assets/images/main_img.png" alt="главное фото">
       </div>
       <div class="main_content">
         <h1>Pyrokinesis</h1>
-        <h2>Люди говорят —  мол, тексты не горят, но я сжигал в них всё и птицей отпускал</h2>
+        <p>Люди говорят —  мол, тексты не <br>горят, но я сжигал в них всё и <br>птицей отпускал</p>
       </div>
     </div>
   </main>
   
-  <!-- <div class="menu">
-
-  </div> -->
- 
+  <section class="page_cards">
+    <div class="card">
+      <img src="../assets/images/Group_photo.png" alt="фотографии">
+      <p>Фотографии</p>
+    </div>
+  </section>
  
 </template>
 
 <style scoped>
 
+
+
 .container {
   display: flex;
-  width: 1440px;
+  max-width: 1440px;
   height: 1000px;
+  margin: 0 auto;
 }
 
 .main_photo {
   position: absolute;
   left: 0;
- 
+  top: 74px;
   width: 100%;
   height: 100%;
-  z-index: 1; 
+  z-index: 1;
 }
 
 .main_photo img {
-  top: 130px;
-  width: 100%;
+  width: 1440px;
   height: 100%;
   object-fit: cover;
+  mask-image: linear-gradient(
+    to bottom,
+    black 0%,
+    black 75%,
+    transparent 100%
+  );
+
 }
+
 
 .main_content{
   position: relative;
   z-index: 2;
+  width: 1440px;
+
 }
 
-/* .main {
-  background-image: url('@/assets/images/main.png');
+.main_content h1{
+  font-family: "Koulen";
+  font-size: 100px;
+  /* left: 795px;
+  top: 629px; */
+  margin: 560px;
+  margin-right: 175px;
+}
 
-} */
+.main_content p{
+   margin-top: -740px;
+   margin-left: 50px;
+   font-size: 36px;
+   line-height: 30px;
+}
+
+.card{
+  margin-top: -250px;
+}
+
+.card p{
+  margin-top: -44px;
+  margin-left: 35px;
+  font-size: 24px;
+}
+
+
+
+.main_page, .page_cards {
+  transform: scale(1);
+  transform-origin: top center;
+}
+
+.container, .main_photo, .main_content, .card {
+  transform: translateZ(0);
+  backface-visibility: hidden;
+  perspective: 1000;
+}
+
+/* Принудительно фиксируем все размеры */
+* {
+  flex-shrink: 0;
+  flex-grow: 0;
+}
+
+/* Отключаем любые анимации при ресайзе */
+@media (max-resolution: 1dppx) {
+  .main_page {
+    will-change: transform;
+  }
+}
+
 </style>
